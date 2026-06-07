b51d5cc123f3aabac438bdea19fd1eae30420ac4
ADZKA-AKN <muhammadadzkarullah@gmail.com>
2026-06-07 20:58:48 +0700

    1

--- src/components/preloader.jsx
+++ src/components/preloader.jsx
@@ -7,7 +7,7 @@ const Loader = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        setTimeout(() => setLoading(false), 1000);
+        setTimeout(() => setLoading(false), 3000);
     }, []);
 
 
